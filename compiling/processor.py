17935721ad2334d00d1c8362f5db08507995f4b6
Ian <iansedano@gmail.com>
2021-11-16 09:39:55 +0100

    starting processor

--- compiling/processor.py
+++ compiling/processor.py
@@ -1,2 +1,17 @@
 """Will apply the actions from the constructs to the parsed tree
 """
+
+from parsing.parser import Node
+import constructs.elements
+
+
+def process_tree(tree):
+    root: Node = tree.root
+
+    def process_branch(branch):
+
+        children: list = branch.children
+
+        children_generator = (child for child in children)
+
+        current_child = next(children_generator, None)
